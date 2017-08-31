fbf6881ae76b7458b2447f768cd5811bb3e8015f
xiaoxin.cao <1216099190@qq.com>
2017-08-31 16:50:36 +0800

    啊啊啊啊啊啊啊啊啊啊啊啊啊啊

---
+++ aaaa/aaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaa
+//
+//  Created by cying on 17/8/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaa/aaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaa
+//
+//  Created by cying on 17/8/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
